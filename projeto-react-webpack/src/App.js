e82251a4c610cba7d30f647aece863b6960576dd
KleberTrugilo <klebertrugilo01@gmail.com>
2022-08-19 13:31:13 -0300

    ProjetoReactWebpack, React-Bootstrap

--- projeto-react-webpack/src/App.js
+++ projeto-react-webpack/src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Alert from 'react-bootstrap/Alert';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Nosso primeiro projeto com ReactJS e Webpack.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <section className="mt-3">
+                <p>{BasicExample()}</p>
+            </section>
+        </div>
+    );
+}
+
+function BasicExample() {
+    return (
+        <>
+        {[
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark',
+        ].map((variant) => (
+            <Alert key={variant} variant={variant}>
+            This is a {variant} alert—check it out!
+            </Alert>
+        ))}
+        </>
+    );
 }
 
 export default App;
